57497dcb9a96972ca13ca958ec32ffff65f5ed8d
Vinoth kumar <120662401+vinothkumar95@users.noreply.github.com>
2023-11-05 19:56:36 +0530

    Update widget.js

--- widget.js
+++ widget.js
@@ -1,4 +1,5 @@
-jQuery(document).ready(function() {
+console.log('widget loaded');
+document.onload = function() {
 	$('body').prepend('<div class="header" id="myHeader"><h2>Thank you for widgets!</h2></div>');
 	$('head').prepend('<style>.header { padding: 10px 16px; background: #555; color: #f1f1f1; } .content { padding: 16px; } .sticky { position: fixed; top: 0; width: 100%} .sticky + .content { padding-top: 102px; }</style>');
 	var header = document.getElementById("myHeader");
