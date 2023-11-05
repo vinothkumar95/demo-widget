35cad1bdf5f04a1704a987fbc40ef368409b7ff2
Vinoth kumar <120662401+vinothkumar95@users.noreply.github.com>
2023-11-05 20:16:22 +0530

    Create widget1.js

---
+++ widget1.js
@@ -0,0 +1,14 @@
+console.log('widget loaded');
+document.onload = function() {
+	$('body').prepend('<div class="header" id="myHeader"><h2>Thank you for widgets!</h2></div>');
+	$('head').prepend('<style>.header { padding: 10px 16px; background: #555; color: #f1f1f1; } .content { padding: 16px; } .sticky { position: fixed; top: 0; width: 100%} .sticky + .content { padding-top: 102px; }</style>');
+	var header = document.getElementById("myHeader");
+	var sticky = header.offsetTop;
+	window.onscroll = function() {
+		if (window.pageYOffset > sticky) {
+			header.classList.add("sticky");
+		} else {
+			header.classList.remove("sticky");
+		}
+	};
+});
